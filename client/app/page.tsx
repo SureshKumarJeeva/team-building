682e55010a4ad68e73c9fcd3570ed019d115f63d
Case-Connect <skumar@legalcaseconnect.com>
2025-08-20 04:57:52 +0530

    UI Update

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -26,25 +26,41 @@ function index(){
         saveAs(blob, "Secret-Santa-Game-Result-"+new Date().getFullYear()+".csv");
       }else{
         const response = await res.json();
+        alert(response.message);
         console.log(response);
       }
   }
   return(
     <>
-      <div className="font-bold text-2xl ">Team Building</div>
-      <div className="font-bold text-1xl ">Secret Santa</div>
-      
-        <form onSubmit={organizeGame}>
-        <div className="grid grid-cols-1 w-2xs gap-2">
-          <input type="file" className="border p-2 rounded bg-amber-700 hover:bg-amber-600 cursor-pointer"
-              onChange={(e)=>{
-                if(e.target.files && e.target.files[0]){
-                  setFile(e.target.files[0]);
-                }
-              }}></input>
-          <button type="submit" className="border p-2 rounded bg-amber-700  hover:bg-amber-600 cursor-pointer">submit</button>
+      <div className="flex flex-row-1fr h-screen  ml-7 mr-7">
+        <div className="w-64 bg-gray-900">
+          <aside >
+            <nav className="flex flex-col font-bold space-y-3 ml-8 mt-5">
+              <a href="/" className="text-amber-700">&#129170; Secret Santa</a>
+              <a href="/">&#129170; Game 2</a>
+              <a href="/">&#129170; Game 3</a>
+              <a href="/">&#129170; Game 4</a>
+            </nav>
+          </aside>
         </div>
-        </form>
+        <div className=" flex-grow justify-center bg-gray-800">
+          <form onSubmit={organizeGame} className="mt-20 ml-20">
+          <p>
+            This accepts a list of employees and their contact email id in a file to process the secret friend assignment<br/>
+            Please select a file
+          </p>
+            <div className="grid grid-cols-1 w-2xs gap-2 mt-10">
+              <input type="file" className="border p-2 rounded bg-amber-700 hover:bg-amber-600 cursor-pointer"
+                  onChange={(e)=>{
+                    if(e.target.files && e.target.files[0]){
+                      setFile(e.target.files[0]);
+                    }
+                  }}></input>
+              <button type="submit" className="border p-2 rounded bg-amber-700  hover:bg-amber-600 cursor-pointer">submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
